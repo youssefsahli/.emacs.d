5bcf066ddab60aba00aecc173e7ab1b0a1432edd
sahli youssef <sahli.youssef@ymail.com>
2024-11-08 00:09:40 +0100

    init

---
+++ custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((citar-bibliography . "~/Projects/Thesis/bib/library.bib")
+     (org-cite-export-processors
+      (t typst))
+     (org-pandoc-options-for-typst
+      (standalone))
+     (typst-ts-compile-options . "--root ~/Projects/Thesis")
+     (typst-ts-watch-options . "--root ~/Projects/Thesis"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
